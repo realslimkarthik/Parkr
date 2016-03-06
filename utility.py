806b8cacace09b42816e7cd15fca4858fcece359
Karthik Hariharan <karthikhariharan13@gmail.com>
2016-03-05 20:39:09 -0600

    added get_availability

--- utility.py
+++ utility.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas
 
 data_path = './data/{0}.csv'
@@ -12,3 +13,17 @@ def get_edges(distance=False):
     else:
         return pandas.read_csv(data_path.format('Edges_FishermansWharf'))
 
+
+def get_availability():
+    df = pandas.read_csv(data_path.format('real_time_data'))
+
+    def convert_to_datetime(date_time_str):
+        date_str, time_str = date_time_str.split()
+        year, month, day = [int(i) for i in date_str.split('-')]
+        time_str, millisecond = time_str.split('.')
+        hour, minute, second = [int(i) for i in time_str.split(':')]
+        millisecond = int(millisecond)
+        return datetime(year, month, day, hour, minute, second, millisecond)
+
+    df['timestamp'] = df['timestamp'].apply(convert_to_datetime)
+    return df
