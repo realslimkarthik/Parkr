fc1c528e869783e3090207b6a6dca6407b0740de
karthikhariharan <karthikhariharan13@gmail.com>
2016-03-07 20:19:54 -0600

    Simulation function implemented. Testing pending

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import googlemaps
 from utility import get_nodes, get_edges, get_availability
+
 
 def get_nodes_within_radius(dest, radius):
     pass
@@ -18,10 +19,18 @@ def get_block_availability(block_id, time):
 
 def get_node_from_block(block_id):
     edges_df = get_edges()
-    block = edges_df[edges_df['block_id'] == block_id].loc[0]
+    block = edges_df[edges_df['node_id'] == block_id].loc[0]
     node1 = block.node_id_1
     node2 = block.node_id_2
     return node1, node2
+
+
+def get_node_id_from_long_lat(longitude, latitude):
+    nodes_df = get_nodes()
+    long_df = nodes_df[nodes_df['longitude'] == longitude & nodes_df['latitude'] = latitude]
+    long_lat_node = long_df.loc[0]
+    node_id = long_lat_node.block_id
+    return node_id
 
 
 def deterministic_grav_pull(block_list, origin, time):
@@ -33,19 +42,14 @@ def deterministic_grav_pull(block_list, origin, time):
     max_force = 0
     for block in block_list:
         node1, node2 = get_node_from_block(block)
-        
-        #block_distance = min(googlemaps.road_distance(origin, node1),
-        #    googlemaps.road_distance(origin, node2))
-
         block_distance = min(get_distance(origin,node1), get_distance(origin,node2))
-
         availability = get_block_availability(block, time)
         block_grav_force[block] = availability / block_distance ** 2
         if max_force < block_grav_force[block]:
             max_force = block_grav_force[block]
             chosen_block = block
 
-    return chosen_block, block_grav_force
+    return chosen_block
 
 
 def get_distance(origin, destination):
@@ -68,9 +72,9 @@ def get_distance(origin, destination):
     return dist_text, dist_value
 
 
-def get_long_lat(block_id):
+def get_long_lat(node_id):
     nodes_df = get_nodes()
-    row = nodes_df[nodes_df['block_id'] == block_id].loc[0]
+    row = nodes_df[nodes_df['node_id'] == node_id].loc[0]
     longitude = row.longitude
     latitude = row.latitude
     long_lat = ['%.6f,%.6f' % (longitude, latitude)]
@@ -82,6 +86,7 @@ def probabilistic_grav_pull(node_list):
 
 def uninformed_search():
     pass
+
 
 #
 # Returned list would be of this form, need to extract and return only what is required once we decide
@@ -104,13 +109,15 @@ def uninformed_search():
 #   'polyline': {'points': 'yiweFj~djVU{COsBEy@AMe@{GA]CWQoCQ}C'}, 
 #   'duration': {'text': '3 mins', 'value': 157}}]
 #
-def get_turn_by_turn_directions(origin, destination):
+
+
+def get_turn_by_turn_directions(origin, destination, departure_time):
     
     dir_list = [];
     gmaps = googlemaps.Client(key="AIzaSyDPxsz5WxM_rqmM6ROL97Gthf48qEk5rs0")
 
-    dir_result = gmaps.directions(get_lat_long(origin),get_lat_long(destination), 
-        mode = "driving", departure_time = datetime.now())
+    dir_result = gmaps.directions(origin,destination, 
+        mode = "driving", departure_time = departure_time)
     dir_result_legs = dir_result.get("legs",None)
 
     #there should be only one since there are no waypoints sent in the request
@@ -121,6 +128,91 @@ def get_turn_by_turn_directions(origin, destination):
     return dir_list    
 
 
-def route_vehicle(origin, destination, time):
+def route_vehicle(origin, destination, directions):
     pass
 
+
+# def get_parking_spot(origin, destination, time, algorithm, radius):
+def get_parking_spot(origin, destination, time, radius):
+    block_list = get_nodes_within_radius(destination, radius)
+    if algorithm == 'd':
+        chosen_block = deterministic_grav_pull(block_list, origin, time)
+    # elif algorithm == 'p':
+    #     chosen_block = probabilistic_grav_pull(block_list, origin, time)
+    else:
+        print('algorithm should be d for deterministic_grav_pull or p for probabilistic_grav_pull')
+        chosen_block = -1
+    
+    return chosen_block
+
+
+def route_vehicle(origin, destination, time):
+    origin_long_lat = get_long_lat(origin)
+    destination_long_lat = get_long_lat(destination)
+    directions = get_turn_by_turn_directions(origin_long_lat, destination_long_lat, time)
+
+    distance = 0
+    
+    for step in directions:
+        seconds = step['duration']['value']
+        time = time + timedelta(seconds=seconds)
+        block_availability = get_block_availability(chosen_block['block_id'], time)
+        if block_availability.available == 0:
+            chosen_block['block_id'] = None
+            while chosen_block['block_id'] is None:
+                chosen_block['block_id'] = get_parking_spot(origin, destination, time, radius)
+                if radius > origin_dest_distance:
+                    break
+                radius += 50
+            
+            if chosen_block['block_id'] is None:
+                print('No available parking spots')
+                distance = -1
+                break
+            else:
+                step_node = get_node_id_from_long_lat(step['end_location']['lng'],
+                    step['end_location']['lat'])
+                node1, node2 = get_node_from_block(chosen_block['block_id'])
+                node1_long_lat = get_long_lat(node1)
+                node2_long_lat = get_long_lat(node2)
+
+                if get_distance(origin_long_lat, node1_long_lat) < get_distance(origin_long_lat, node2_long_lat):
+                    chosen_block['node_id'] = node1
+                else:
+                    chosen_block['node_id'] = node2
+                new_distance = route_vehicle(step_node, chosen_block['node_id'], time)
+                if new_distance != -1:
+                    distance += new_distance
+                    break
+        distance += step['distance']['value']
+
+    return distance
+
+
+def simulate(origin, destination, time, algorithm):
+    chosen_block = {}
+    radius = 100
+    chosen_block['block_id'] = None
+    origin_long_lat = get_long_lat(origin)
+    destination_long_lat = get_long_lat(destination)
+
+    origin_dest_distance = get_distance(origin_long_lat, destination_long_lat)
+    
+    while chosen_block['block_id'] is None:
+        chosen_block['block_id'] = get_parking_spot(origin, destination, time, radius)
+        if radius > origin_dest_distance:
+            break
+        radius += 50
+    
+    node1, node2 = get_node_from_block(chosen_block['block_id'])
+    node1_long_lat = get_long_lat(node1)
+    node2_long_lat = get_long_lat(node2)
+
+    if get_distance(origin_long_lat, node1_long_lat) < get_distance(origin_long_lat, node2_long_lat):
+        chosen_block['node_id'] = node1
+    else:
+        chosen_block['node_id'] = node2
+
+    distance = route_vehicle(origin, chosen_block['node_id'], time)
+
+    return distance
