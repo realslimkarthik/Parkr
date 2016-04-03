519ae4c9bd4562e66913162aa1288a10d710894e
Karthik Hariharan <karthikhariharan13@gmail.com>
2016-04-03 18:38:37 -0500

    Refactoring. Moved most get_* functions to utility

--- utility.py
+++ utility.py
@@ -23,8 +23,11 @@ def get_block_list():
     return block_ids['block_id']
 
 
-def get_availability():
-    df = pandas.read_csv(data_path.format('real_time_data'))
+def get_availability(with_time=False):
+    if with_time:
+        df = pandas.read_csv(data_path.format('real_time_data_with_time'))
+    else:
+        df = pandas.read_csv(data_path.format('real_time_data'))
 
     def convert_to_datetime(date_time_str):
         date_str, time_str = date_time_str.split()
@@ -60,3 +63,81 @@ def get_distance(origin, destination):
         dist_value = elem_list[0].get("distance").get("value")
             
     return dist_text, dist_value
+
+
+def get_block_availability(block_id, time):
+    availability_df  = get_availability()
+    block_availability_df = availability_df[availability_df['block_id'] == block_id]
+    block_availability_timed_df = block_availability_df[block_availability_df['timestamp'] == time]
+    if block_availability_timed_df.empty:
+        block_availability_timed_df = block_availability_df[block_availability_df['timestamp'] < time]
+        last_block = block_availability_timed_df.max().available
+    return last_block
+
+
+def get_node_from_block(block_id):
+    edges_df = get_edges()
+    block = edges_df[edges_df['block_id'] == block_id].iloc[0]
+    node1 = block.node_id_1
+    node2 = block.node_id_2
+    return node1, node2
+
+
+def get_node_id_from_long_lat(longitude, latitude):
+    nodes_df = get_nodes()
+    long_df = nodes_df[nodes_df['longitude'] == longitude & nodes_df['latitude'] == latitude]
+    long_lat_node = long_df.iloc[0]
+    node_id = long_lat_node.block_id
+    return node_id
+
+
+def get_long_lat(node_id):
+    nodes_df = get_nodes()
+    row = nodes_df[nodes_df['node_id'] == node_id]
+    longitude = row.longitude
+    latitude = row.latitude
+    long_lat = [float(longitude), float(latitude)]
+    return long_lat
+
+
+def get_distance(node1, node2):
+    distance_df = get_distances()
+    row = distance_df[(distance_df['node_id_1'] == node1) & (distance_df['node_id_2'] == node2)].iloc[0]
+    distance = row.distance
+    return distance
+
+
+def _value_in_datetime(datetime_object, value, attribute):
+    attribute = attribute.lower()
+    value_in_datetime = False
+    existing_value = getattr(datetime_object, attribute)
+    
+    if existing_value == value:
+        value_in_datetime = True
+
+    return value_in_datetime
+
+
+
+def get_block_probability(block_id, time, fine_grained=True):
+    day = time.day
+    weekday = time.weekday()
+    hour = time.hour
+    minute = time.minute
+    availability_df = get_availability(with_time=True)
+    block_df = availability_df[availability_df['block_id'] == block_id]
+    if fine_grained:
+        day_df = block_df[block_df['weekday'] == weekday]
+    else:
+        working_days = range(0, 5)
+        weekends = range(5, 7)
+        if weekday in working_days:
+            day_df = block_df[block_df['weekday'].isin(working_days)]
+        else:
+            day_df = block_df[block_df['weekday'].isin(weekends)]
+
+    day_time_df = day_df[day_df['hour'] == hour]
+    probability = (day_time_df['available'] / day_time_df['no_blocks']).mean()
+
+    return probability
+
